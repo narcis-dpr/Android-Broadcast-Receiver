28daaea1ebd383aec21d4b2d5b31d79411191706
n.dipir <nargesdeypir@gmail.com>
2024-01-23 16:56:31 +0330

    recycler view -- add Recycler view to activity and show items

--- app/src/main/java/com/narcis/broadcast/recyclerView/RecyclerActivity.kt
+++ app/src/main/java/com/narcis/broadcast/recyclerView/RecyclerActivity.kt
@@ -2,14 +2,25 @@ package com.narcis.broadcast.recyclerView
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.LayoutManager
 import com.narcis.broadcast.databinding.ActivityRecyclerBinding
 
 class RecyclerActivity : AppCompatActivity() {
     private lateinit var mBinding: ActivityRecyclerBinding
+
+    private var layoutManager: RecyclerView.LayoutManager? = null
+    private var adapter: RecyclerView.Adapter<RecyclerAdapter.ViewHolder>? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mBinding = ActivityRecyclerBinding.inflate(layoutInflater)
         setContentView(mBinding.root)
 
+        layoutManager = LinearLayoutManager(this)
+        mBinding.recyclerList.layoutManager = layoutManager
+        adapter = RecyclerAdapter()
+        mBinding.recyclerList.adapter = adapter
     }
 }
